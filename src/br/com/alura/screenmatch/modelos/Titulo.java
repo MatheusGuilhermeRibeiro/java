c2465f128278d93aad377a96390c77495ff84db3
Matheus <matheusguilhermeribeirodev@gmail.com>
2025-01-07 16:09:26 -0300

    Curso alura JAVA

---
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -0,0 +1,49 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Titulo {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int duracaoEmMinutos;
+    private int totalAvaliacoes;
+
+    public int getTotalAvaliacoes() {
+        return totalAvaliacoes;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme : " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    public void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalAvaliacoes++;
+    }
+
+    public double pegaMedia() {
+        return somaDasAvaliacoes / totalAvaliacoes;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+}
